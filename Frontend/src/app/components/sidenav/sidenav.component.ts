3095ea40df12fbc45467c6980e789539aab68331
Kevin Giron <kevin.giron000@gmail.com>
2019-06-03 20:44:32 -0600

    se implementó buscador en sidenav _kgiron

--- Frontend/src/app/components/sidenav/sidenav.component.ts
+++ Frontend/src/app/components/sidenav/sidenav.component.ts
@@ -11,6 +11,41 @@ export class SidenavComponent implements OnInit {
 
   ngOnInit() {
     
+
+    $(document).ready(function(){
+
+
+      $('#buscador').keyup(function(){
+  
+
+         var mantenimientos = $('.mantenimiento');
+         var buscando = $(this).val();
+         var item='';
+         for( var i = 0; i < mantenimientos.length; i++ ){
+             item = $(mantenimientos[i]).html().toLowerCase();
+              for(var x = 0; x < item.length; x++ ){
+                  if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
+
+                    $(mantenimientos[i]).parents('#item').show(); 
+                    //document.getElementById('notFound').style.display = 'none';  
+                      
+                  }
+                  /*else if( item.indexOf( buscando ) == -1) {  //busca en una cadena string -- lo que se introduce
+                    $(mantenimientos[i]).parents('#item').hide(); 
+                    document.getElementById('notFound').style.display = 'block';  
+                  }*/
+                                    
+                  else{
+
+                    $(mantenimientos[i]).parents('#item').hide(); 
+                                      
+                       
+                  }
+              }
+         }
+      });
+    });
+    
   }
 
 }
